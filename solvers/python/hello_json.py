4781bb6dd5e1b4223e5a9f3db1483953eced9ec9
Victor Klochikhin <vintlucky777@gmail.com>
2023-07-06 09:59:11 -0700

    figured out python modules and relative imports

--- solvers/python/hello_json.py
+++ solvers/python/hello_json.py
@@ -2,12 +2,17 @@ import sys
 import json
 from src.utils import meaning_of_life
 
-if len(sys.argv) < 2:
-    print('Usage: python ./solver.py PROBLEM_ID')
-    exit(1)
+def solve(problem_id=None):
+    problem_id = problem_id or (len(sys.argv) >= 2 and sys.argv[1])
+    if not problem_id:
+        print('Usage: python solver.py PROBLEM_ID')
+        exit(1)
 
-with open(f'problems/{sys.argv[1]}.json', 'r') as file:
-    problem = json.loads(file.read())
+    with open(f'problems/{sys.argv[1]}.json', 'r') as file:
+        problem = json.loads(file.read())
 
-print("Hello json!")
-print(json.dumps(problem, indent=2))
+    print("Hello json!")
+    print(json.dumps(problem, indent=2))
+
+if __name__ == '__main__':
+    solve()

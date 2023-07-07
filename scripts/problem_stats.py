63b9861f1cd77989e828c352f5d4d3dd69f09784
Oleg Godovykh <0legg@users.noreply.github.com>
2023-07-07 16:00:53 -0700

    Add assertion for taste dimensions

--- scripts/problem_stats.py
+++ scripts/problem_stats.py
@@ -1,6 +1,7 @@
 import json
 import itertools
 import os
+from types import SimpleNamespace
 
 print("problem musicians attendees tastes [w x h]")
 
@@ -10,17 +11,19 @@ for i in itertools.count(start=1):
         break
     with open(path, "r") as f:
         data = f.read()
-        js = json.loads(data)
-        # print(js.keys())
-        tastes = len(set(js["musicians"]))
+        js = json.loads(data, object_hook=lambda d: SimpleNamespace(**d))
+        tastes_musicians = len(set(js.musicians))
+        tastes_attendees = set(len(x.tastes) for x in js.attendees)
+        assert len(tastes_attendees) == 1
+        assert tastes_musicians == next(iter(tastes_attendees))
         print(
             "{:2} -> {:5} {:5} {:5} [{} x {}] ".format(
                 i,
-                len(js["musicians"]),
-                len(js["attendees"]),
-                tastes,
-                int(js["stage_width"]),
-                int(js["stage_height"]),
-                # len(js["pillars"]),
+                len(js.musicians),
+                len(js.attendees),
+                tastes_musicians,
+                int(js.stage_width),
+                int(js.stage_height),
+                # len(js.pillars),
             )
         )

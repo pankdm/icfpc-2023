45fd1ee0f2433c151571fde47aafd7e9ea9ef0c1
Victor Klochikhin <vintlucky777@gmail.com>
2023-07-10 02:29:12 -0700

    new solutions

---
+++ solvers/cpp/build_run_and_submit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -ex
+# make
+# #executing args command
+# $@
+./main.solver -mode update -solution vintlucky
+git add .
+git commit -m 'new solutions'
+git fetch
+git rebase origin/master
+git push

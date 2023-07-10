07a2366f5e653b6bd7ab56925308da4a25cb97d3
Dmitry Panin <pankdm@gmail.com>
2023-07-10 01:36:11 -0700

    Solutions for y offset

--- scripts/update_best_meta.sh
+++ scripts/update_best_meta.sh
@@ -2,4 +2,4 @@
 pushd solvers/cpp
 make compile && ./main.solver -mode eval_json -solution loks_best
 popd
-python scripts/solution_previews.py && gca -m "new previews" && gm && gp
+python scripts/solution_previews.py && gca -m "new previews + latest score" && gm && gp

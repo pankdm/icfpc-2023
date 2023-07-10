75c32889353d223e05c00f3223d9d2c57e64f2b9
Dmitry Panin <pankdm@gmail.com>
2023-07-09 17:36:36 -0700

    new previews

--- scripts/monitor_leaderboard.sh
+++ scripts/monitor_leaderboard.sh
@@ -4,5 +4,5 @@ while true; do
     echo "Loop iteration"
     PYTHONPATH=. python3 scripts/leaderboard.py
     date
-    sleep 600  # Sleep for 10 minutes
+    sleep 60  # Sleep for 10 minutes
 done

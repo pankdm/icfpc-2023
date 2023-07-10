bd50ab87497418c97226d8a8f14889fb04b1f23c
Victor Klochikhin <vintlucky777@gmail.com>
2023-07-10 02:46:34 -0700

    new solutions

--- solvers/cpp/build_run_and_submit.sh
+++ solvers/cpp/build_run_and_submit.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -ex
+
 make
-#executing args command
-$@
-./main.solver -mode update -solution vintlucky
+# #executing args command
+# $@
+# ./main.solver -mode $MODE -solver $SOLVER  -nthreads $NTHREADS -first_problem $FIRST_PROBLEM -last_problem $LAST_PROBLEM -timelimit $TIMELIMIT
+# ./main.solver -mode update -solution vintlucky
 git add ../..
 git commit -m 'new solutions'
 git fetch

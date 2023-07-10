65d78fd9882f17a6a4ab6502d84a17f37131e55f
Dmitry Panin <pankdm@gmail.com>
2023-07-09 23:34:20 -0700

    Run border script

---
+++ scripts/run_border.sh
@@ -0,0 +1,10 @@
+# make compile && ./main.solver -mode run -solver border  -nthreads 4 -first_problem 1  -last_problem 11 -layers 3 -offset 3
+# make compile && ./main.solver -mode run -solver border  -nthreads 4 -first_problem 1  -last_problem 11 -layers 3 -offset 4
+# make compile && ./main.solver -mode run -solver border  -nthreads 4 -first_problem 46  -last_problem 55 -layers 3 -offset 1
+# make compile && ./main.solver -mode run -solver border  -nthreads 4 -first_problem 46  -last_problem 55 -layers 3 -offset 2
+
+make compile && ./main.solver -mode run -solver border  -nthreads 4 -first_problem 28  -last_problem 55 -layers 3 -offset 1
+make compile && ./main.solver -mode run -solver border  -nthreads 4 -first_problem 28  -last_problem 55 -layers 3 -offset 2
+make compile && ./main.solver -mode run -solver border  -nthreads 4 -first_problem 28  -last_problem 55 -layers 3 -offset 3
+make compile && ./main.solver -mode run -solver border  -nthreads 4 -first_problem 28  -last_problem 55 -layers 3 -offset 4
+make compile && ./main.solver -mode run -solver border  -nthreads 4 -first_problem 28  -last_problem 55 -layers 3 -offset 5
